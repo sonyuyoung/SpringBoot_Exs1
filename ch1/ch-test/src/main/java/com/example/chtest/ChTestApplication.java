777b0102bccc44eb94ac82c286d26d7e11741fb1
02 <sonyuyoung@naver.com>
2023-09-25 18:51:41 +0900

    first commit

---
+++ ch1/ch-test/src/main/java/com/example/chtest/ChTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.chtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChTestApplication.class, args);
+    }
+
+}
---
+++ ch1/ch-test/src/test/java/com/example/chtest/ChTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
